6979d31a1cb77a2dd14e21bb36b3cb3b27525df1
Ronald Benjamin <dodo.ben.rb@gmail.com>
2023-11-17 00:07:52 +0530

    python math

---
+++ 09_python_modules/06_python_math/01_python_math.py
@@ -0,0 +1,41 @@
+# Python Math
+# python has a set of built-in math functions, including an extensive math module.
+# allows you to perform mathematical task on numbers.
+
+# Built-in Math function
+# min() and max() function can be used to find the minimum and maximum number respectively.
+
+x = min(5,65,632)
+print(x)
+
+x = max(5,65,632)
+print(x)
+
+# abs() function returns the absolute (positive) value of the specified number
+x = abs(-23.333)
+print(x)
+
+# pow() function returns the power of x to y
+x = pow(4,5)
+print(x)
+
+# Math Module
+# built-in module <math>, extends the list of mathematical function.
+# we can use methods and constant of the module
+import math
+
+x = math.sqrt(64)
+print(x)
+
+# math.ceil() -> rounds to upward nearest number
+x= math.ceil(1.4)
+print(x)
+
+# math.floor() -> rounds to lower nearest number
+x = math.floor(1.4)
+print(x)
+
+# math.pi constant value
+
+x = math.pi
+print(x)
